6acc1ff28093e1cd3401b5b515050cb88bba8e19
Ehtiram-Ullah <ehtiramullah786@gmail.com>
2024-10-09 13:37:04 +0500

    Add common background and choose course page.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-const text = "Are you ready to begin the journey ?"; // The text you want to show
-const typingSpeed = 100; // Speed of typing (in milliseconds)
-let index = 0;
+//$ For prog_basics page 
+// const text = "Are you ready to begin the journey ?"; // The text you want to show
+// const typingSpeed = 100; // Speed of typing (in milliseconds)
+// let index = 0;
 
-function typeText() {
-    if (index < text.length) {
-        document.getElementById("typing-text").innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeText, typingSpeed);
-    } else {
-        // Show buttons after typing effect is complete
-        document.querySelector('.cursor').style.display = 'none'; // Hide the cursor
-        document.getElementById("buttons-container").classList.remove('hidden'); // Show buttons
-    }
-}
+// function typeText() {
+    //     if (index < text.length) {
+        //         document.getElementById("typing-text").innerHTML += text.charAt(index);
+        //         index++;
+        //         setTimeout(typeText, typingSpeed);
+        //     } else {
+            //         // Show buttons after typing effect is complete
+            //         document.querySelector('.cursor').style.display = 'none'; // Hide the cursor
+            //         document.getElementById("buttons-container").classList.remove('hidden'); // Show buttons
+            //     }
+            // }
+            
+            // // Start typing effect on page load
+            // window.onload = function () {
+                //     typeText();
+                // };
+//$Ends For prog_basics page 
 
-// Start typing effect on page load
-window.onload = function () {
-    typeText();
-};
+
+//$ for background code animation 
+
+document.addEventListener("DOMContentLoaded",function(){
+    fetch('/components/background.html').then(response => response.text()).then(data =>{
+        console.log("Background code is "+ data.length);
+        document.getElementById('background-code').innerHTML = data;
+        console.log("Done");
+        // alert("Hello");
+    });
+});
+
+//$Ends for background code animation 
